128e42391591332ad8dafa6c87299df8bcacd763
Sara Gochez <saragochez13@gmail.com>
2022-01-24 01:04:57 -0800

    added new google fonts link and modifed buttons

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+var currentDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//  // Button for clear tasks
+//  $("#clearTasks").on("click", function(){
+//     localStorage.clear();
+//     // this is where the hourly function should be called : functionName)
+//   }) 
