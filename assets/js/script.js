7f93e436080facf50213196302236971c19eaf5a
Sara Gochez <saragochez13@gmail.com>
2022-01-26 22:18:12 -0800

    added vars to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,15 +2,18 @@ var hour = moment().format('h:mm:ss a');
 var todaysDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 
 
-
-
-
-
-
-
-
-
-
+var eight = $("#eight");
+var nine = $("#nine");
+var ten = $("#ten");
+var eleven = $("#eleven");
+var twelve = $("#twelve");
+var one = $("#one");
+var two = $("#two");
+var three = $("#three");
+var four = $("#four");
+var five = $("#five");
+var six = $("#six");
+var seven = $("#seven");
 
 
 
@@ -21,7 +24,6 @@ var todaysDate = moment().format('dddd') + " " + moment().format("Do MMM YYYY");
 
 
  // Button for clear tasks
- $("#clearTasks").on("click", function(){
+ $("#clearTasks").on("click", function() {
     localStorage.clear();
-    // this is where the hourly function should be called : functionName)
   }) 
