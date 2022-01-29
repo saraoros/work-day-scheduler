5a5376236db5162d6cb98088263d31673ec3cc1e
Sara Gochez <saragochez13@gmail.com>
2022-01-29 00:52:45 -0800

    got localStorage and items to link

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ var currentDay =
   moment().format('dddd') + ' ' + moment().format('MMMM DD YYYY');
 var hourSpan;
 
-var userInput= " ";
+var userInput = ' ';
 var eight = $('#eight');
 var nine = $('#nine');
 var ten = $('#ten');
@@ -18,67 +18,94 @@ var five = $('#five');
 var six = $('#six');
 var seven = $('#seven');
 
-
-
 function timeSlots() {
-    var slotEight = JSON.parse(localStorage.getItem('#eight')); eight.val(slotEight);
+  var slotEight = JSON.parse(localStorage.getItem('08:00 AM'));
+  eight.val(slotEight);
 
-    var slotNine = JSON.parse(localStorage.getItem('#nine')); nine.val(slotNine);
+  var slotNine = JSON.parse(localStorage.getItem('09:00 AM'));
+  nine.val(slotNine);
 
-    var slotTen = JSON.parse(localStorage.getItem('#ten')); ten.val(slotTen);
+  var slotTen = JSON.parse(localStorage.getItem('10:00 AM'));
+  ten.val(slotTen);
 
-    var slotEleven = JSON.parse(localStorage.getItem('#eleven')); eleven.val(slotEleven);
+  var slotEleven = JSON.parse(localStorage.getItem('11:00 AM'));
+  eleven.val(slotEleven);
 
-    var slotTwelve = JSON.parse(localStorage.getItem('#twelve')); twelve.val(slotTwelve);
+  var slotTwelve = JSON.parse(localStorage.getItem('12:00 PM'));
+  twelve.val(slotTwelve);
 
-    var slotOne = JSON.parse(localStorage.getItem('#one')); one.val(slotOne);
+  var slotOne = JSON.parse(localStorage.getItem('01:00 PM')); 
+  one.val(slotOne);
 
-    var slotTwo = JSON.parse(localStorage.getItem('#two')); two.val(slotTwo);
+  var slotTwo = JSON.parse(localStorage.getItem('02:00 PM'));
+  two.val(slotTwo);
 
-    var slotThree = JSON.parse(localStorage.getItem('#three')); three.val(slotThree);
+  var slotThree = JSON.parse(localStorage.getItem('03:00 PM'));
+  three.val(slotThree);
 
-    var slotFour = JSON.parse(localStorage.getItem('#four')); four.val(slotFour);
+  var slotFour = JSON.parse(localStorage.getItem('04:00 PM'));
+  four.val(slotFour);
 
-    var slotFive = JSON.parse(localStorage.getItem('#five')); five.val(slotFive);
+  var slotFive = JSON.parse(localStorage.getItem('05:00 PM'));
+  five.val(slotFive);
 
-    var slotSix = JSON.parse(localStorage.getItem('#six')); six.val(slotSix);
+  var slotSix = JSON.parse(localStorage.getItem('06:00 PM'));
+  six.val(slotSix);
 
-    var slotSeven = JSON.parse(localStorage.getItem('#seven')); seven.val(slotSeven);
+  var slotSeven = JSON.parse(localStorage.getItem('07:00 PM'));
+  seven.val(slotSeven);
 
-console.log("currentHour" + hour + userInput);
+  console.log('currentHour' + hour + userInput);
+}
+
+  var interval = setInterval(function () {
+    var momentNow = moment();
+    $('#currentDay').html(
+      momentNow.format('YYYY MMMM DD') + ' ' + momentNow.format('dddd')
+    );
+    $('#currentDay').html(currentDay + ' ' + momentNow.format('hh:mm:ss A'));
+  }, 100);
+
+
+  
+
+function timeCheck() {
+  $('.form-control').each(function() {
+     var time = parseInt($(this).attr('id'));
+     if (hour > time) {
+       $(this).addClass('.past');
+     } else if (hour < time) {
+       $(this).addClass('.future');
+     } else {
+       $(this).addClass('.present');
+     }
+  });
 }
 
 
-$(document).ready(function(){
-  timeSlots();
-  // formFunction();
 
 
-$(".btn").on("click", function(){
+
+
+  $('.btn').on('click', function () {
     localStorage.setItem(hourSpan, JSON.stringify(userInput));
-})
+    userInput = $(this).siblings(".form-control").val().trim();
+    hourSpan = $(this).siblings(".input-group-prepend").text().trim();
+    console.log(hourSpan);
+    console.log(userInput);
+  });
 
 
 
+$(document).ready(function () {
+  timeSlots();
+  timeCheck();
 
 
 
-
-var interval = setInterval(function() {
-  var momentNow = moment();
-  $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
-  + momentNow.format('dddd'));
-  $('#currentDay').html(currentDay + " " + momentNow.format('hh:mm:ss A'));
-}, 100);
-
-
-
-
-
-//Button for clear tasks
-$("#clearTasks").on("click", function() {
+  //Button for clear tasks
+  $('#clearTasks').on('click', function () {
     localStorage.clear();
     timeSlots();
-})
-
+  });
 });
