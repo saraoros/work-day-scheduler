b680a9fd9620c38ca68f00f633e7312fc824cc82
Sara Gochez <saragochez13@gmail.com>
2022-01-28 21:43:28 -0800

    added timeSlots function

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,46 @@ var five = $('#five');
 var six = $('#six');
 var seven = $('#seven');
 
+
+
+function timeSlots() {
+
+var slotEight = JSON.parse(localStorage.getItem('#eight')); eight.val(slotEight);
+
+var slotNine = JSON.parse(localStorage.getItem('#nine')); nine.val(slotNine);
+
+var slotTen = JSON.parse(localStorage.getItem('#ten')); ten.val(slotTen);
+
+var slotEleven = JSON.parse(localStorage.getItem('#eleven')); eleven.val(slotEleven);
+
+var slotTwelve = JSON.parse(localStorage.getItem('#twelve')); twelve.val(slotTwelve);
+
+var slotOne = JSON.parse(localStorage.getItem('#one')); one.val(slotOne);
+
+var slotTwo = JSON.parse(localStorage.getItem('#two')); two.val(slotTwo);
+
+var slotThree = JSON.parse(localStorage.getItem('#three')); three.val(slotThree);
+
+var slotFour = JSON.parse(localStorage.getItem('#four')); four.val(slotFour);
+
+var slotFive = JSON.parse(localStorage.getItem('#five')); five.val(slotFive);
+
+var slotSix = JSON.parse(localStorage.getItem('#six')); six.val(slotSix);
+
+var slotSeven = JSON.parse(localStorage.getItem('#seven')); seven.val(slotSeven);
+
+console.log("currentHour" + hour);
+}
+
+timeSlots();
+
+
+$(".saveBtn").on("click", function(){
+    localStorage.setItem(hourSpan, JSON.stringify(userInput));
+})
+
+
+
 var interval = setInterval(function() {
   var momentNow = moment();
   $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
@@ -25,7 +65,12 @@ var interval = setInterval(function() {
   $('#currentDay').html(currentDay + " " + momentNow.format('hh:mm:ss A'));
 }, 100);
 
-//  // Button for clear tasks
-//  $("#clearTasks").on("click", function() {
-//     localStorage.clear();
-//   })
+
+
+
+
+//Button for clear tasks
+$("#clearTasks").on("click", function() {
+    localStorage.clear();
+    timeSlots();
+})
