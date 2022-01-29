dd954fee59612065c6502ce15196f4121df2be5c
Sara Gochez <saragochez13@gmail.com>
2022-01-28 20:09:45 -0800

    got the time and date to work properly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,31 @@
 var hour = moment().hours();
 var currentHour = moment().format('h:mm:ss a');
-var todaysDate = moment().format('dddd') + " " + moment().format("MMMM DD YYYY");
+var currentDay =
+  moment().format('dddd') + ' ' + moment().format('MMMM DD YYYY');
 var hourSpan;
 
 var userInput;
-var eight = $("#eight");
-var nine = $("#nine");
-var ten = $("#ten");
-var eleven = $("#eleven");
-var twelve = $("#twelve");
-var one = $("#one");
-var two = $("#two");
-var three = $("#three");
-var four = $("#four");
-var five = $("#five");
-var six = $("#six");
-var seven = $("#seven");
+var eight = $('#eight');
+var nine = $('#nine');
+var ten = $('#ten');
+var eleven = $('#eleven');
+var twelve = $('#twelve');
+var one = $('#one');
+var two = $('#two');
+var three = $('#three');
+var four = $('#four');
+var five = $('#five');
+var six = $('#six');
+var seven = $('#seven');
 
-var date = setInterval (function() {
- var currentTime = moment();
- $("#todaysDate").html(currentTime.format("MMMM DD YYYY") + " " + currentTime.format("dddd").substring(0,3).toUpperCase());
- //$("#todaysDate").html(currentHour + '' + currentTime.format('hh:mm:ss A'));
-  }, 100);
-
-
-
-
+var interval = setInterval(function() {
+  var momentNow = moment();
+  $('#currentDay').html(momentNow.format('YYYY MMMM DD') + ' '
+  + momentNow.format('dddd'));
+  $('#currentDay').html(currentDay + " " + momentNow.format('hh:mm:ss A'));
+}, 100);
 
 //  // Button for clear tasks
 //  $("#clearTasks").on("click", function() {
 //     localStorage.clear();
-//   }) 
+//   })
